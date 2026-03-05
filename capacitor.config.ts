c357f87b25f89a7c6e134ca955bc1a423af5be51
Matias Galeano <99203645+matigaleanodev@users.noreply.github.com>
2026-03-05 09:50:44 -0300

    chore(android): incorporación de plataforma android y ajustes de ignorados locales

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.matiasgaleano.modoplayapp',
+  appId: 'io.modoplay.app',
   appName: 'Modo Playa',
   webDir: 'www',
 };
