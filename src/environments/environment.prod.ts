a314397cbfde5cdba1aa87d4507bddc5d3a9c182
Matias Galeano <99203645+matigaleanodev@users.noreply.github.com>
2026-03-29 15:45:48 -0300

    chore(release): actualizacion de version 1.0.4

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   API_URL: 'https://api-playa.matiasgaleano.dev/api',
-  appVersion: '1.0.3',
+  appVersion: '1.0.4',
   appStage: 'Produccion',
 };
